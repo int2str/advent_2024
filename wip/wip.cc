1ef65d953b2735396eb3e8fccd9c6a47eed2ab1b
Andre Eisenbach <git@4ae.us>
2024-12-09 22:15:24 -0500

    Added work-in-progess template

---
+++ wip/wip.cc
@@ -0,0 +1,18 @@
+//
+// int2str's Advent of Code 2024
+// https://adventofcode.com/2024/day/??
+//
+
+#include <fmt/core.h>
+
+#include "utils/read_file.hh"
+
+namespace DayWIP {
+//
+
+}  // namespace DayWIP
+
+auto main() -> int {
+  const auto file = Utils::readFile("wip/sample.txt");
+  // ...
+}
