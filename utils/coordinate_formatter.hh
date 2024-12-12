ecdddf36002437e48a63e3c1dc2a8cb14eb3ee07
Andre Eisenbach <git@4ae.us>
2024-12-12 14:34:40 -0500

    Added format specifier to Coordinate formatter

--- utils/coordinate_formatter.hh
+++ utils/coordinate_formatter.hh
@@ -7,16 +7,50 @@
 
 template <>
 struct fmt::formatter<Utils::Coordinate> {
+  enum class View : uint8_t { Raw_XY, Editor_YX };
+
   template <typename ParseContext>
   constexpr auto parse(ParseContext& ctx) {
-    return ctx.begin();
+    auto it = ctx.begin();
+    while (it != ctx.end() and *it != '}') {
+      if (*it == 'e') {
+        view_ = View::Editor_YX;
+        base_ = 1;
+
+      } else if (*it == 'r') {
+        view_ = View::Raw_XY;
+        base_ = 0;
+
+      } else {
+        if !consteval {
+          throw fmt::format_error(
+              fmt::format("Invalid Coordinate format specifier '{}'", *it));
+        }
+        break;
+      }
+      ++it;
+    }
+    return it;
   }
 
   template <typename FormatContext>
   auto format(const Utils::Coordinate& coordinate, FormatContext& ctx) const {
-    return fmt::format_to(ctx.out(), "{}/{}", coordinate.y + 1,
-                          coordinate.x + 1);
+    switch (view_) {
+      case View::Editor_YX:
+        return fmt::format_to(ctx.out(), "{}:{}", coordinate.y + base_,
+                              coordinate.x + base_);
+      case View::Raw_XY:
+        return fmt::format_to(ctx.out(), "{}/{}", coordinate.x + base_,
+                              coordinate.y + base_);
+      default:
+        throw fmt::format_error("Invalid coordinate format");
+    }
+    return ctx.out();
   }
+
+ private:
+  View view_{View::Raw_XY};
+  int base_{0};
 };
 
 #endif  // COORDINATE_FORMATTER_HH
