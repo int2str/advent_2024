b3ad7b3062e29b7c00b1dbf7faf1c1fafb025c94
Andre Eisenbach <git@4ae.us>
2024-12-14 22:08:20 -0500

    Giving the CTRE library a try

--- 14/robots.cc
+++ 14/robots.cc
@@ -1,40 +1,25 @@
 #include "robots.hh"
 
+#include <fmt/core.h>
+
 #include <filesystem>
 #include <ranges>
 #include <string>
 #include <vector>
 
+#include "external/ctre.hpp"
 #include "utils/coordinate.hh"
 #include "utils/nm_view.hh"
 #include "utils/read_file.hh"
-#include "utils/split.hh"
 
 namespace Day14::Internal {
 
-[[nodiscard]] auto filterLine(const std::string& line) -> std::string {
-  const auto no_pv = [](char ch) {
-    return ch != 'p' and ch != 'v' and ch != '=';
-  };
-  const auto space_comma = [](char ch) { return ch == ' ' ? ',' : ch; };
-  return line                                  //
-         | std::views::filter(no_pv)           //
-         | std::views::transform(space_comma)  //
-         | std::ranges::to<std::string>();
-}
-
-[[nodiscard]] auto makeRobot(const std::string& filtered) -> Robots {
-  const auto make_coordinate = [](const auto& chunk) {
-    return Utils::Coordinate{*chunk.begin(), *std::next(chunk.begin())};
-  };
-  const auto make_robot = [](auto position, auto velocity) {
-    return Robot{position, velocity};
-  };
-  return Utils::split<int>(filtered, ",")          //
-         | std::views::chunk(2)                    //
-         | std::views::transform(make_coordinate)  //
-         | std::views::pairwise_transform(make_robot) |
-         std::ranges::to<std::vector>();
+[[nodiscard]] auto buildRobot(const std::string& line) -> Robot {
+  using namespace ctre::literals;  // NOLINT
+  auto [_, px, py, vx, vy] =
+      ctre::match<R"(p=(\d+),(\d+)\s+v=(-?\d+),(-?\d+))">(line);
+  return Robot{.position = {px.to_number(), py.to_number()},
+               .velocity = {vx.to_number(), vy.to_number()}};
 }
 
 }  // namespace Day14::Internal
@@ -43,9 +28,7 @@ namespace Day14 {
 
 auto robotsFromFile(const std::filesystem::path& path) -> Robots {
   return Utils::readLines(path)                         //
-         | std::views::transform(Internal::filterLine)  //
-         | std::views::transform(Internal::makeRobot)   //
-         | std::views::join                             //
+         | std::views::transform(Internal::buildRobot)  //
          | std::ranges::to<std::vector>();
 }
 
